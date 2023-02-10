3da7341e56f56a9a46cf231b619c651aa2a764e1
Hidayatullah Ahsan <hahsan@techicon.net>
2023-02-09 18:52:51 -0600

    First version that works
    The library and console application build. The console application is
    simple and provides no user interaction. Its purpose is to help a user
    get started with the library.

--- VodConsole/Program.cs
+++ VodConsole/Program.cs
@@ -1,1 +1,20 @@
-﻿
+﻿using VodLibrary;
+using System.Xml;
+
+string uri = "./examples/incorrect-ssp.xml";
+
+ValidateOscalDocuments v = new();
+v.Validate(uri);
+if (!v.IsValid)
+{
+	Console.WriteLine($"{uri} is an invalid document");
+	Console.WriteLine(@"The errors are: ");
+	foreach (var item in v.ValidationErrors)
+	{
+		Console.WriteLine($"{item.Severity} | {item.Error}");
+	}
+}
+else
+{
+	Console.WriteLine($"{uri} is a valid document");
+}
